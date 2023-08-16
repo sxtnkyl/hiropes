558fc928aca07d5dbbe01fd8ee3be844920d192a
kyle sexton <ksexton.exe@gmail.com>
2023-08-16 16:45:28 -0400

    modularize imports

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts'],
+  // This may need refactored once mui support for nextjs13 completed
+  // initial issue raised
+  // https://github.com/mui/material-ui/issues/35840
+  // open MUI issue
+  // https://github.com/mui/material-ui/issues/34905
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+    '@emotion/server': {
+      transform: '@emotion/server/{{member}}',
+    },
+  },
 };
 
 module.exports = nextConfig;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme, responsiveFontSizes } from '@mui/material';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 const theme = responsiveFontSizes(
   createTheme({
